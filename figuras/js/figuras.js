bb75dd418e4f7806fb037012ce125e897ce47b6f
montesp <pavelmontes22@gmail.com>
2022-01-12 23:01:54 -0700

    Se añadio el diseño de la pagina de formulas de figuras geometricas

--- figuras/js/figuras.js
+++ figuras/js/figuras.js
@@ -1,84 +1,48 @@
-//Codigo cuadrado
-// console.group("Cuadrado");
-
-// const ladoCuadrado = 5;
-// console.log(`Los lados del cuadrado miden: ${ladoCuadrado} cm`);
-
+//Funciones cuadrado
 function perimetroCuadrado(lado) {
     return lado * 4;
 }
-// console.log(`EL perimetro del cuadrado es: ${perimetroCuadrado(ladoCuadrado)} cm `);
 
 function areaCuadrado(lado) {
     return lado * lado;
 }
-// console.log(`EL area del cuadrado es: ${areaCuadrado(ladoCuadrado)} cm `);
 
-// console.groupEnd();
-
-//Codigo triangulo
-// console.group("Triangulos");
-
-// const ladoTriangulo1 = 6;
-// const ladoTriangulo2 = 6;
-// const baseT = 4;
-// const alturaT = 5.5;
-
-// console.log(`Los lados del triangulo miden: ${ladoTriangulo1} cm, ${ladoTriangulo2} cm y ${baseT} cm`);
-// console.log(`La altura del triangulo mide: ${alturaT} cm`);
-
+//Funciones triangulo
 function perimetroTriangulo(lado1, lado2, lado3) {
     return lado1 + lado2 + lado3;
 }
-// console.log(`EL perimetro del triangulo es: ${perimetroTriangulo(ladoTriangulo1, ladoTriangulo2, baseT)} cm `); 3
+
 
 function areaTriangulo(base, altura) {
     return (base * altura) / 2;
 }
-// console.log(`EL area del triangulo es: ${areaTriangulo(baseT, alturaT)} cm cuadrados `);
 
-// console.groupEnd();
-
-//Codigo circulo
-
-// console.group("Circulos");
-
-// const radio = 4;
-// console.log(`El radio es: ${radio} cm`);
+//Funciones circulo
+const PI = Math.PI;
 
 function diametro(r) {
     return r * 2;
 }
 
-// console.log(`El diametro es: ${diametro(radio)} cm`);
-
-const PI = Math.PI;
-// console.log(`El valor PI es: ${PI} cm`);
-
-
 function perimetroCirculo(r) {
     const dm = diametro(r);
     return dm * PI;
 }
-// console.log(`EL area del circulo es: ${perimetroCirculo(radio)} cm cuadrados `);
-
 
 function areaCirculo(r) {
     return (r * r) * PI;
 }
 
-// console.log(`EL area del circulo es: ${areaCirculo(5)} cm cuadrados `);
-
-
-// console.groupEnd();
-
+//Funciones HTML para el cuadrado
 function calcularPerimetroCuadrado() {
     const input = document.getElementById("InputCuadrado")
     const value = input.value;
     console.log(value);
 
     const perimetro = perimetroCuadrado(value);
-    alert(perimetro);
+    const resultado = "Resultado: " + perimetro;
+    document.getElementById("result").innerHTML = resultado;
+    // alert(perimetro);
 }
 
 function calcularAreaCuadrada() {
@@ -88,3 +52,4 @@ function calcularAreaCuadrada() {
     const area = areaCuadrado(value);
     alert(area);
 }
+//Reto platzi añadirle una funcion a nuestra pagina
