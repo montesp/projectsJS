4f2fd75f382cd11b07f654ce7092e3dae1ae07c0
montesp <pavelmontes22@gmail.com>
2022-01-13 21:43:39 -0700

    Se agrego  el nuevo proyecto donde el mediante una pagina un usuario va a agregar un precio y un descuento, asi el sistema calculara cual sera el precio que tendra el sistema con descuento

---
+++ PorcentajesDescuentos/js/descuentos.js
@@ -0,0 +1,40 @@
+
+ const precioOriginal = 120;
+ const descuento = 18;
+
+
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount(){
+    const precio = parseFloat(document.getElementById("InputPrice").value);
+
+    const descuento = parseFloat(document.getElementById("InputDiscount").value);
+
+
+
+    if(descuento > 5 && descuento < 95){
+        const resultadoPrecioConDescuento = calcularPrecioConDescuento(precio, descuento);
+
+        document.getElementById("priceP").innerText = "El precio con descuento es " + resultadoPrecioConDescuento;
+
+        
+    } else {
+        alert("No se puede calcular ese descuento, ingresa de nuevo el descuentyo")
+    }
+
+   
+}
+
+//  console.log({
+//      precioOriginal,
+//      descuento,
+//      porcentajePrecioConDescuento,
+//      precioConDescuento
+//  });
