9b640fe5ec309ee69051c4065218a116a621bc05
Pavel Montes <pavelmontes22@gmail.com>
2022-02-24 16:14:06 -0700

    Ejercicio del scope global

---
+++ scope-closure/src/scope/global.js
@@ -0,0 +1,16 @@
+var hello = "Hello";
+var hello = 'Hello +'
+let world = "Hello world";
+// let world = "Hello";
+const helloWorld = "Hello World!"
+
+
+
+const anotherFuntion = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+
+}
+
+anotherFuntion();
