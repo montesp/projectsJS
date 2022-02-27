b33cb47b6be04003c3c5e140901eacc6aa340d7a
Pavel Montes <pavelmontes22@gmail.com>
2022-02-26 20:06:56 -0700

    Ejercicio de debuggin usando devTools

---
+++ scope-closure/src/debug/index.js
@@ -0,0 +1,13 @@
+var a = 'Hello';
+
+function hello(){
+    let b = "Hello world!";
+    const c = 'Hello world!';
+
+    if(true){
+        let d = 'Hello world!!';
+    }
+    debugger
+}
+
+hello();
