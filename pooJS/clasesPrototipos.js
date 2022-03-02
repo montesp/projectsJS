c6c04e26f0a28eefdaf6295769fdaba6f7691653
Pavel Montes <pavelmontes22@gmail.com>
2022-03-01 20:46:42 -0700

    Se creo una clase nueva para crear rutas de aprendizaje dentro de la clase

--- pooJS/clasesPrototipos.js
+++ pooJS/clasesPrototipos.js
@@ -1,7 +1,14 @@
 class LearningPaths {
     constructor({
-        
-    })
+        name,
+        career,
+        curses = [],
+    }) {
+        this.name = name;
+        this.career = career;
+        this.curses = curses;
+    }
+
 }
 
 class Student {
@@ -28,14 +35,48 @@ class Student {
     };
 }
 
+
+
+// RETO: hacer una clase de learning paths y añadirla 
+const desarrolloWeb = new LearningPaths({
+    name: "Escuela de desarrollo web",
+    career: "Desarrollo e Ingeneria",
+    curses: [
+        "Cursos definitivo de HTML y CSS",
+        "Curso profesional de JS",
+        "Curso introduccion a React.js"
+    ],
+});
+
+const diseñoGrafico = new LearningPaths({
+    name: "Escuela de diseño grafico",
+    career: "Diseño y UX",
+    curses: [
+        "Cursos de Illustrator",
+        "Curso de Dibujo de retrato",
+        "Curso de arte para personajes 2D"
+    ],
+});
+
+const marketing = new LearningPaths({
+    name: "Escuela de marketing digital",
+    career: "Marketing",
+    curses: [
+        "Curso de introduccion al marketing digital",
+        "Curso de community Manager",
+        "Curso de marca personal",
+    ]
+});
+
 const carlos = new Student({
     name: "Carlos",
     username: "criptobro",
     email: "criptobro22@gmail.com",
     instagram: "elCriptobro",
-
+    learningPaths: [
+        desarrolloWeb,
+        marketing,
+    ],
 });
 
 console.log(carlos);
-
-// RETO: hacer una clase de learning paths y añadirla 
