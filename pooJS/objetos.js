a2ee81d0da803540eff6a231d8546a4a7f661398
Pavel Montes <pavelmontes22@gmail.com>
2022-03-07 21:04:04 -0700

    copiar objetos con diferentes meteodos (shallow copy)

--- pooJS/objetos.js
+++ pooJS/objetos.js
@@ -1,26 +1,44 @@
-const pavel = {
-    name:'pavel',
-    age: 17,
-    approvedCourses: ["Curso1"],
-    addCourse(newCourse) {
-        this.approvedCourses.push(newCourse);
+// const pavel = {
+//     name:'pavel',
+//     age: 17,
+//     approvedCourses: ["Curso1"],
+//     addCourse(newCourse) {
+//         this.approvedCourses.push(newCourse);
+//     }
+// }
+
+// // console.log(Object.keys(pavel));
+// // console.log(Object.getOwnPropertyNames(pavel));
+// // console.log(Object.entries(pavel));
+
+
+// // Object.defineProperty(pavel, "pruebaNASA", {
+// //     value: "Extraterrestres",
+// //     writable: false,
+// //     enumerable: false,
+// //     configurable: false,
+// // });
+
+// Object.seal(pavel) // Hace que todas las propiedades configurable sea false  (Se eliminen)
+// Object.freeze(pavel) // Hace que todas las propiedades writable sea false  (Se cambie los valores)
+
+
+// console.log(Object.getOwnPropertyDescriptors(pavel));
+//Shallow copy
+const obj1 = {
+    a:"a",
+    b:"b",
+    c:{
+        d:"d",
+        e:"e",
     }
 }
 
-// console.log(Object.keys(pavel));
-// console.log(Object.getOwnPropertyNames(pavel));
-// console.log(Object.entries(pavel));
+const obj2 = {};
+for(prop in obj1){
+    obj2[prop] = obj1[prop];
+}
 
 
-// Object.defineProperty(pavel, "pruebaNASA", {
-//     value: "Extraterrestres",
-//     writable: false,
-//     enumerable: false,
-//     configurable: false,
-// });
-
-Object.seal(pavel) // Hace que todas las propiedades configurable sea false  (Se eliminen)
-Object.freeze(pavel) // Hace que todas las propiedades writable sea false  (Se cambie los valores)
-
-
-console.log(Object.getOwnPropertyDescriptors(pavel));
+const obj3 = Object.assign({}, obj1);
+const obj4 = Object.create(obj1);
