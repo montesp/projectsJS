5873ead97231e518598f841c277bde214a4b0c61
Pavel Montes <pavelmontes22@gmail.com>
2022-03-14 20:47:50 -0700

    ecmascript 7

---
+++ ecmascript/src/es7/index.js
@@ -0,0 +1,12 @@
+let numbers = [1,2,3,7,8];
+if(numbers.includes(4)){
+    console.log('Si se encuentra el valor 7')
+} else {
+    console.log('No se encuentra');
+}
+
+let base = 4;
+let exponente = 3;
+let result = base ** exponente;
+
+console.log(result);
