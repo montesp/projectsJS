db949b9889b66d00468ae9db42d1f0d75874a37b
Pavel Montes <pavelmontes22@gmail.com>
2022-07-25 20:37:59 -0600

    Array methods  for each

---
+++ arrayManipulation/server/for-each.js
@@ -0,0 +1,12 @@
+const letters = ['a', 'b', 'c'];
+//for
+for (let index = 0; index < letters.length; index++){
+    const element = letters[index]
+    console.log(element)
+}
+
+const letter = ['a', 'b', 'c', 'd'];
+
+letter.forEach(item => {
+    console.log(item)
+});
