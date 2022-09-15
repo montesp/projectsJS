3f7717c20e7b0cd2f8d201f7572ef77d626f33b6
Pavel Montes <pavelmontes22@gmail.com>
2022-09-14 22:57:49 -0600

    the image container loads a dog using TheDogApi

--- API/js/index.js
+++ API/js/index.js
@@ -1,1 +1,13 @@
-console.log("Works");
+const URL = "https://api.thedogapi.com/v1/images/search";
+
+
+//Un boton para recargar la imagen
+//Usar asyncs await
+
+fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        const imageContainer  = document.getElementById("image-container");
+        console.log(imageContainer)
+        imageContainer.src = data[0].url;
+    });
