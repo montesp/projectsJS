2d9504cb9a5600966261ebab79bf020c3790a926
Pavel Montes <pavelmontes22@gmail.com>
2022-09-19 21:47:34 -0600

    Landing created with async/await function and css

--- API/js/index.js
+++ API/js/index.js
@@ -1,13 +1,19 @@
 const URL = "https://api.thedogapi.com/v1/images/search";
+const button = document.getElementById("button");
 
+button.addEventListener('click', () => {
+    getDogImage(URL);
+})
 
-//Un boton para recargar la imagen
-//Usar asyncs await
-
-fetch(URL)
+async function getDogImage(url){
+    await fetch(url)
     .then(response => response.json())
     .then(data => {
         const imageContainer  = document.getElementById("image-container");
         console.log(imageContainer)
         imageContainer.src = data[0].url;
     });
+}
+//Un boton para recargar la imagen
+//Usar asyncs await
+getDogImage(URL);
