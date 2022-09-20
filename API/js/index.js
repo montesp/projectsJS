8b2accccc632edc5acc2f033668fd6b8b7730d96
Pavel Montes <pavelmontes22@gmail.com>
2022-09-19 22:27:19 -0600

    API request using query parameters

--- API/js/index.js
+++ API/js/index.js
@@ -1,5 +1,6 @@
-const URL = "https://api.thedogapi.com/v1/images/search";
+const URL = "https://api.thedogapi.com/v1/images/search?limit=3";
 const button = document.getElementById("button");
+
 
 button.addEventListener('click', () => {
     getDogImage(URL);
@@ -8,10 +9,14 @@ button.addEventListener('click', () => {
 async function getDogImage(url){
     const res = await fetch(url);
     const data = await res.json();
-    const imageContainer  = await document.getElementById("image-container");
-    console.log(imageContainer)
-    imageContainer.src = data[0].url;
+    const imageContainer1  = await document.getElementById("image-container1");
+    const imageContainer2  = await document.getElementById("image-container2");
+    const imageContainer3  = await document.getElementById("image-container3");
 
+    console.log(data)
+    imageContainer1.src = data[0].url;
+    imageContainer2.src = data[1].url;
+    imageContainer3.src = data[2].url;
 }
 
 getDogImage(URL);
