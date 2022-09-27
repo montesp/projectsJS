a1c33f9f1be59c207a1313f171f2d31827243f83
Pavel Montes <pavelmontes22@gmail.com>
2022-09-26 23:24:30 -0600

    Page shows dogs utilize the DogAPI

--- API/src/js/index.js
+++ API/src/js/index.js
@@ -1,4 +1,4 @@
-const URL = "https://api.thedogapi.com/v1/images/search?limit=3&api_key=live_NFimpQ2CBVkNM9eRyX35O6KkYkF5lYTFBTR92SWrKDVXlblL7d4HXw1nHQ9YQwSm";
+const URL = "https://api.thedogapi.com/v1/images/search?limit=6&api_key=live_NFimpQ2CBVkNM9eRyX35O6KkYkF5lYTFBTR92SWrKDVXlblL7d4HXw1nHQ9YQwSm";
 const button = document.getElementById("button");
 
 
@@ -12,11 +12,17 @@ async function getDogImage(url){
     const imageContainer1  = await document.getElementById("image-container1");
     const imageContainer2  = await document.getElementById("image-container2");
     const imageContainer3  = await document.getElementById("image-container3");
+    const imageContainer4  = await document.getElementById("image-container4");
+    const imageContainer5  = await document.getElementById("image-container5");
+    const imageContainer6  = await document.getElementById("image-container6");
 
-    console.log(data)
     imageContainer1.src = data[0].url;
     imageContainer2.src = data[1].url;
     imageContainer3.src = data[2].url;
+    imageContainer4.src = data[3].url;
+    imageContainer5.src = data[4].url;
+    imageContainer6.src = data[5].url;
+
 }
 
 getDogImage(URL);
