35c1aed870cda98f04dabcc78edfa12d79772912
Pavel Montes <pavelmontes22@gmail.com>
2023-02-09 17:21:30 -0600

    create shift and unshift methods in array classs

--- dataStructure/array.js
+++ dataStructure/array.js
@@ -42,7 +42,7 @@ class MyArray {
     this.shiftIndex(index);
     return deletedItem;
   }
-  private shiftIndex(index){
+  shiftIndex(index){
     for (let i = index; i < this.length - 1; i++) {
       this.data[i] = this.data[i + 1]
     }
@@ -50,12 +50,22 @@ class MyArray {
     this.length--;
   }
   // Add an element in the beggining
-  unshift(){
-
+  unshift(element){
+    for (let i = this.length; i > 0; i--) {
+      this.data[i] = this.data[i-1];
+    }
+    this.data[0] = element;
+    this.length++;
+    return this.data;
   }
   // Delete an element in the beggining
   shift(){
-    
+    for (let i = 0; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length -1];
+    this.length--;
+    return this.data;
   }
 }
 
