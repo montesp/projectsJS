67da8e5e8bc995ac2b1ee4c938b8c36108c13e76
Pavel Montes <pavelmontes22@gmail.com>
2023-02-09 17:00:57 -0600

    create delete and pop method in array class

--- dataStructure/array.js
+++ dataStructure/array.js
@@ -14,23 +14,48 @@ class MyArray {
     this.length++;
     return this.data;
   }
+  // pop(){
+  //   if(this.length !== 0){
+  //     const temporal = {};
+  //     for (let index = 0; index < this.length; index++) {
+  //       if(index !== this.length - 1){
+  //         temporal[index] = this.data[index]
+  //       }
+  //     }
+  //     this.data = {
+  //       ...temporal,
+  //     };
+  //     this.length--;
+  //   } else {
+  //     console.log("Dont have any value")
+  //   }
+  //   return this.data;
+  // }
   pop(){
-    if(this.length !== 0){
-      let temporal = {};
-      for (let index = 0; index < this.length; index++) {
-        if(index !== this.length - 1){
-          // console.log(`Valor${index}`)
-          temporal[index] = this.data[index]
-        }
-      }
-      this.data = {
-        ...temporal,
-      };
-      this.length--;
-    } else {
-      console.log("Dont have any value")
+    const lastItem = this.data[this.length - 1];
+    delete this.data[this.length -1];
+    this.length--;
+    return lastItem;
+  }
+  delete(index){
+    const deletedItem = this.data[index];
+    this.shiftIndex(index);
+    return deletedItem;
+  }
+  private shiftIndex(index){
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1]
     }
-    return this.data;
+    delete this.data[this.length - 1];
+    this.length--;
+  }
+  // Add an element in the beggining
+  unshift(){
+
+  }
+  // Delete an element in the beggining
+  shift(){
+    
   }
 }
 
