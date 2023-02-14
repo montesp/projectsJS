7dc02e9a77dc29e89ec622077610c240eb300a99
Pavel Montes <pavelmontes22@gmail.com>
2023-02-13 18:15:55 -0600

    hash tables class started

---
+++ dataStructure/hashTables.js
@@ -0,0 +1,13 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  hashMethod(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+}
