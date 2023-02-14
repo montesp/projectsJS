e4645d8e802578a01f21ca6247f1385a867d18a2
Pavel Montes <pavelmontes22@gmail.com>
2023-02-13 21:19:28 -0600

    hashTable search and insert method was created

--- dataStructure/hashTables.js
+++ dataStructure/hashTables.js
@@ -10,4 +10,27 @@ class HashTable {
     }
     return hash;
   }
+  insert(key, value) {
+    const address = this.hashMethod(key);
+    if(!this.data[address]) {
+      this.data[address] = [];
+    }
+    this.data[address].push([key, value]);
+    return this.data;
+  }
+  search(key){
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[address];
+
+    if(currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if(currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
+
+const myHashTable = new HashTable(50);
