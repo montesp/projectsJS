c84bd3de8206a34470ccb17ff9d81d3638328d0a
Pavel Montes <pavelmontes22@gmail.com>
2023-02-14 23:28:35 -0600

    linked list file created

---
+++ dataStructure/singleLinkList.js
@@ -0,0 +1,37 @@
+// let singlyLinkedList =  {
+//   head: {
+//     value: 1,
+//     next: {
+//       value:2
+//       next: {
+//         value: 3,
+//         next: {
+//           value:4,
+//           next: null
+//         }
+//       }
+//     }
+//   }
+// }
+
+class Node {
+  constructor(value){
+    this.value = value,
+    this.next = null;
+  }
+}
+
+class MySinglyLinkedList {
+  constructor(value){
+    this.head = {
+      value: value,
+      next: null,
+    }
+    this.tail = this.head;
+    this.length = 1;
+  }
+
+
+}
+
+let myLinkedList = new MySinglyLinkedList(1);
