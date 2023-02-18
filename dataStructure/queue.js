182feeb267aa4b5ad45a8fa25f31a68cc2c6afb7
Pavel Montes <pavelmontes22@gmail.com>
2023-02-17 23:13:47 -0600

    Queue class file has been created

---
+++ dataStructure/queue.js
@@ -0,0 +1,28 @@
+class Node {
+  constructor(value){
+    this.value = value,
+    this.prev = null;
+  }
+}
+
+
+class Queue {
+  constructor(){
+    this.first = null;
+    this.last = null;
+    this.length = 0;
+
+  }
+
+  enqueue(){
+    // Add element at the end
+  }
+
+  dequeue(){
+    // remove first element
+  }
+
+  peek(){
+    // get first element
+  }
+}
