32e45102a4982f4c8bcc6b1486c38348ae667202
Pavel Montes <pavelmontes22@gmail.com>
2023-02-18 01:18:14 -0600

    Queue methods dequeue, enqueue and peek done

--- dataStructure/queue.js
+++ dataStructure/queue.js
@@ -14,15 +14,36 @@ class Queue {
 
   }
 
-  enqueue(){
+  enqueue(value){
     // Add element at the end
+    const newNode =  new Node(value);
+    if(this.length === 0){
+      this.first =  newNode;
+      this.last = newNode;
+    } else {
+      this.last.prev = newNode;
+      this.last = newNode;
+    }
+    this.length++;
+    return this;
   }
 
   dequeue(){
     // remove first element
+    if(this.length === 1){
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.prev;
+    }
+    this.length--;
+    return this;
   }
 
   peek(){
     // get first element
+    return this.first;
   }
 }
+
+const myQueue = new Queue();
